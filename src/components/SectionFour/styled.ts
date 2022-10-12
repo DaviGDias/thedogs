aa29877906ae2b41cd70b40c3f9648f80fa40cf9
DaviDias <davihkd@hotmail.com>
2022-10-12 18:34:47 -0300

    Update styled.ts

--- src/components/SectionFour/styled.ts
+++ src/components/SectionFour/styled.ts
@@ -6,7 +6,7 @@ export const Container = styled.section`
     align-items: center;
     justify-content: center;
     width: 100%;
-    background: ${(props) => props.theme['white']};
+    background: ${(props) => props.theme['orange-100']};
     height: 90vh;
 `;
 
